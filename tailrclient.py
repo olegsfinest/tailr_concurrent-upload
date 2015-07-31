8b54137a8c4bb736e5ea92abab6d0c8ff71b5977
paul wille <paul.wille@t-online.de>
2015-07-31 14:36:05 +0200

    brackets are cut off for key

--- tailrclient.py
+++ tailrclient.py
@@ -29,7 +29,7 @@ failedRequestsUrls = {}
 concurrencyLimit = 150
 #config
 userName = "olegsfinest"
-repoName = "test6"
+repoName = "test9"
 tailrToken = "6c8369343aa0754741225b6f161bb7f0bcd1d388"
 contentType = "application/n-triples"
 
@@ -71,20 +71,27 @@ def processFile(fsrcpath):
 			if (line.strip() and line.strip()[0] != '#'):
 				s, p, o, g = processQuad(line)
 				if (currentGraph != g):
-					if (currentGraph != ''):
+					if (g in graphContents.keys()):
+						logging.error("-- Duplicate graph: " + g)
+					elif (currentGraph != ''):
 						# logging.debug("-- Write " + currentGraph + "\n" + "\n".join(graphContents[currentGraph]))
-						push(currentGraph, ("\n".join(graphContents[currentGraph])), pool)
+
+						key = currentGraph
+						if checkForBrackets(key):
+							key = cutoffBrackets(key)
+
+						push(key, ("\n".join(graphContents[currentGraph])), pool)
+
 						addUrl(currentGraph)
 						graphContents[currentGraph].clear()
 						numberOfGraphs += 1
-						
+
 						# # Nummber of Connections Logging
 						# global currentConnections
 						# currentConnections = currentConnections + 1
 						# logging.debug(" === Current Connections: " + str(currentConnections) + "\n")
 
-					if (g in graphContents.keys()):
-						logging.error("-- Duplicate graph: " + g)
+					
 					currentGraph = g
 
 				graphContents[currentGraph].add(s + " " + p + " " + o + " .")
@@ -93,6 +100,10 @@ def processFile(fsrcpath):
 			# 	break
 			# i = i + 1
 		# logging.debug("-- Write " + currentGraph + "\n" + "\n".join(graphContents[currentGraph]))
+
+		# wait for all reqests to finish
+		pool.join()
+
 		print("\n")
 		logging.info("## Finished pushing " + str(numberOfGraphs) + " graphs")
 		logging.info("## " + str(numberOfGraphs - len(failedRequestsUrls))+ " requests were succesfull")
@@ -115,6 +126,11 @@ def push(key, payload, pool):
 	grequests.send(req, pool)
 
 
+def pushWithFile(key, filePath, pool):
+	params={'key':key}
+	req = grequests.put(apiURI, params=params, headers=header, data=open(filePath, 'rb'), hooks={'response': printResponse})
+	grequests.send(req, pool)
+
 def printResponse(response, *args, **kwargs) :
 	# print (response.url +" returned status code: " + str(response.status_code))
 	global currentConnections
@@ -127,10 +143,21 @@ def addUrl(url):
 	with open(urlOutputfileName, 'a') as file:
 		file.write(url+"\n")
 
+# currently not used
+# this would require a list with all urls. urls are currently appended to file right away with addUrl()
 def saveUrls(urls):
 	with open(urlOutputfileName, 'a') as file:
 		for url in urls:
 			file.write(url+"\n")
+
+
+def checkForBrackets(s):
+	if s[0] == "<" and s[-1] == ">":
+		return True
+	return False
+
+def cutoffBrackets(s):
+	return s[1:len(s)-1]
 
 def printFailedRequests():
 	for k, v in failedRequestsUrls:
